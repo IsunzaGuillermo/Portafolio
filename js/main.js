8b16bb96f4f0744bd37c43cf4ddf530b15167e35
IsunzaGuillermo <isunzaguillermo@gmail.com>
2022-09-29 14:04:39 -0500

    Se hizo un total rediseño y reestructuración aplicando mis conocimientos con un mejor entendimiento

--- js/main.js
+++
@@ -1,5 +0,0 @@
-alert ("Hello me monke' welcome")
-
-nombre=prompt("U give name?")
-
-console.log (nombre)
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+let toTopBtn = document.getElementById("tohome");
+
+function toTop () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
